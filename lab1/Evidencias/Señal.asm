6d5c83fa959de05efb9d653c51ba65d04b0f0a04
Mateo Lecuna <mateo.lecuna@estudiantes.utec.edu.uy>
2025-09-09 13:29:41 -0300

    Simplificación del código para la señal

--- lab1/Evidencias/Señal.asm
+++ lab1/Evidencias/Señal.asm
@@ -19,7 +19,8 @@ RESET:
     out PORTD, r16
 
 MAIN_LOOP:
-	rcall senal
+	rcall arriba
+    rcall abajo
     rjmp MAIN_LOOP
 
 Delay:
@@ -34,197 +35,28 @@ L1: dec  r20
     brne L1
     nop
 
-Senal:
-	ldi r16, 0x00
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x08
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x10
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x18
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x20
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x28
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x30
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x38
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x40
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x48
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x50
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x58
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x60
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x68
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x70
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x78
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x80
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x88
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x90
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x98
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xa0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xa8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xb0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xb8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xc0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xc8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xd0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xd8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xe0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xe8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xf0
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xf8
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xff
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xf7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xef
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xe7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xdf
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xd7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xcf
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xc7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xbf
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xb7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xaf
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0xa7
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x9f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x97
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x8f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x87
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x7f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x77
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x6f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x67
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x5f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x57
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x4f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x47
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x3f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x37
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x2f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x27
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x1f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x17
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x0f
-	out POTRD, r16
-	rcall delay
-	ldi r16, 0x07
-	out POTRD, r16
-	rcall delay
+arriba:
+	ldi r16, 0b00000000
+	ldi r17, 0b11111000
+	ldi r18, 0b00001000
+	out PORTD, r16
+suma:
+	rcall delay
+	add r16, r18
+	out PORTD, r16
+	cp r16, r17
+	brge suma
+	ret
+
+abajo:
+	ldi r16, 0b11111111
+	ldi r17, 0b00000111
+	ldi r18, 0b00001000
+	out PORTD, r16
+resta:
+	rcall delay
+	sub r16, r18
+	out PORTD, r16
+	cp r17, r16
+	brge resta
 	ret
