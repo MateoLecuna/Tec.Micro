063f85b89a7499f77d99a29cdc4ea08492faa6c0
Mateo Lecuna <mateo.lecuna@estudiantes.utec.edu.uy>
2025-10-20 02:00:16 -0300

    Reordanado de archivos en evidencias

--- lab2/1 Plotter/Plotter/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-#include <stdint.h>
-
-void commands_init(void);
-void commands_process_byte(uint8_t b);
-void commands_poll(void); // llamado desde main loop (no bloquea)
-
-#endif
