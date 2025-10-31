7a3a2cb082089d022c005364a5470af1d72c416d
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-31 13:36:39 -0300

    Completados movimientos y Autohome
    UP (dist), DWN (dist), L (dist) y R (dist)
    home()
    
    
    home() solo afecta el eje Y porque no hay limites en el eje X  :(

--- lab3/1 Plotter/Plotter/Plotter/main.c
+++ lab3/1 Plotter/Plotter/Plotter/main.c
@@ -18,6 +18,8 @@
 #define PASO1R		0x38
 #define PASO2R		0x30
 
+#define LED 		0x20
+
 
 #define motDelay	10
 
@@ -58,32 +60,95 @@ int main (void){
 //										FUNCIONES
 ////////////////////////////////////////////////////////////////////////////////////////////
 
+//					Movimientos
+/////////////////////////////////////////////////////
 
+//		Eje Y
+///////////////////
 void UP (int dist){
 	for(int i = 0 ; i <= dist ; i++){
-		PORTC |= PASO1UP;
-		_delay_ms(motDelay);
-		PORTC &= 0xF7				// ~0b0000 1000 (CLK Y)
-		_delay_ms(motDelay);
+		if (PIND & 0x08){				// Chequea que el carro Y no supere el limite YD
+			PORTC |= PASO1UP;			// Pone en 1 todos los bits que mueven el motor
+			_delay_ms(motDelay);
+			PORTC &= 0xF7				// ~0b0000 1000 (CLK Y) Apaga la señal de reloj
+			_delay_ms(motDelay);
+		} else {
+			break;
+		}
 	}
 }
 
 void DWN (int dist){
+	for(int i = 0 ; i <= dist ; i++){	// Da la cantidad de pasos definida por el usuario
+		if (PIND & 0x04){				// Chequea que el carro Y no supere el limite YA
+			PORTC |= PASO1DWN;
+			_delay_ms(motDelay);
+			PORTC &= 0xF7				// ~0b0000 1000 (CLK Y)
+			_delay_ms(motDelay);
+		} else {
+			break;
+		}
+	}
+}
+
+//		Eje X
+/////////////////////
+
+void L (int dist){
 	for(int i = 0 ; i <= dist ; i++){
-		PORTC |= PASO1DWN;
-		_delay_ms(motDelay);
-		PORTC &= 0xF7				// ~0b0000 1000 (CLK Y)
-		_delay_ms(motDelay);
+			PORTB |= PASO1L;			// Pone en 1 todos los bits que mueven el motor
+			_delay_ms(motDelay);
+			PORTB &= 0xF7				// ~0b0000 1000 (CLK Y) Apaga la señal de reloj
+			_delay_ms(motDelay);
+	}
+}
+
+void R (int dist){
+	for(int i = 0 ; i <= dist ; i++){
+			PORTB |= PASO1R;			// Pone en 1 todos los bits que mueven el motor
+			_delay_ms(motDelay);
+			PORTB &= 0xF7				// ~0b0000 1000 (CLK Y) Apaga la señal de reloj
+			_delay_ms(motDelay);
 	}
 }
 
 
+//					Autohome
+//////////////////////////////////////////////////////
 void home(void){
-	while((PIND & 0x04)){
-		if (!(PIND & 0x04)){
-			PORTD = 0x20;
-			} else {
-			PORTD = 0;
+	uint16_t d = 0;
+
+	
+	PORTD |= LED;			// Prende y apaga el LED
+	_delay_ms(300);
+	PORTD &= ~LED;
+	_delay_ms(150);
+	PORTD |= LED;	
+	_delay_ms(300);
+	PORTD &= ~LED;
+	_delay_ms(1000);
+
+	
+	while((PIND & 0x04)){		// Paso 1: Baja hasta encontrar el limite inferior (YA)
+		if ((PIND & 0x04)){
+			DOWN(1);
+		} else {
+			break;
 		}
 	}
+
+	
+	while((PIND & 0x08)){		// Paso 2: Sube hasta encontrar el limite superior (YD)
+		if ((PIND & 0x08)){
+			UP(1);
+			d++					// Recopila la distancia recorrida
+		} else {
+			break;
+		}
+	}
+
+	for (int i = 0 ; i <= (d/2) ; i++){		// Paso 3: Baja hasta la mitad del trayecto para ubicarse en el centro
+		DOWN(1);
+	}
+	
 }
