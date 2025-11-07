5b7a2ae8e654cff306dc3ec1a1684ce97206c563
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-11-06 21:02:34 -0300

    Corrección de errores, código funcional
    48 errores solucionados, ahora funciona la función home();

--- lab3/1 Plotter/Plotter/Plotter/main.c
+++ lab3/1 Plotter/Plotter/Plotter/main.c
@@ -1,28 +1,13 @@
-#define F_CPU 1000000L
+#define F_CPU 1000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <math.h>
-
-
+#include <stdint.h>
 
 //								DEFINICIONES
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-#define PASO1DWN	0x28
-#define PASO2DWN	0x20
-#define PASO1UP		0x38
-#define PASO2UP		0x30
-
-#define PASO1L		0x28
-#define PASO2L		0x20
-#define PASO1R		0x38
-#define PASO2R		0x30
-
-#define LED 		0x20
-
 #define motDelay	1      	// Delay en ms
-
 
 //				Selector de máquina
 /////////////////////////////////////////////////////
@@ -30,37 +15,37 @@
 ///////////////////
 #define machine 0
 
-#define clkX 	PD2
-#define dirX 	PD5
-#define enX 	PB0
-#define sol 	PB3
-#define clkY	PD3
-#define dirY	PD6
-#define enY		PB0
-#define limYA	PB2
-#define limYD	PB1
-#define LED		PB5
+#define clkX   (1<<PD2)
+#define dirX   (1<<PD5)
+#define enX    (1<<PB0)
+#define sol    (1<<PB3)
+#define clkY   (1<<PD3)
+#define dirY   (1<<PD6)
+#define enY    (1<<PB0)
+#define limYA  (1<<PB2)
+#define limYD  (1<<PB1)
+#define LED    (1<<PB5)
 
-
-//	Plotter lab
-///////////////////
-/*
-#define machine 1
-
-#define clkX 	PB3
-#define dirX 	PB4
-#define enX		PB5
-#define sol		PC0
-#define clkY	PC3
-#define dirY	PC4
-#define enY		PC5
-#define limYA	PD2
-#define limYD	PD3
-#define LED		PD5
+/* Plotter lab config (kept for reference)
+#define clkX   (1<<PB3)
+#define dirX   (1<<PB4)
+#define enX    (1<<PB5)
+#define sol    (1<<PC0)
+#define clkY   (1<<PC3)
+#define dirY   (1<<PC4)
+#define enY    (1<<PC5)
+#define limYA  (1<<PD2)
+#define limYD  (1<<PD3)
+#define LED    (1<<PD5)
 */
 
 //										FUNCIONES
 ////////////////////////////////////////////////////////////////////////////////////////////
+
+// small helper
+static inline void step_delay(void){
+	_delay_ms(motDelay);
+}
 
 //					Movimientos
 /////////////////////////////////////////////////////
@@ -68,98 +53,92 @@
 //		Eje Y
 ///////////////////
 void UP (int dist){
-	if (machine == 0){		// Chequea qué máquina es
-		PORTD =| dirY;
-		PORTB =| enY;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTD =| clkY;
-			_delay_ms(1);
-			PORTD =& ~clkY;
-			_delay_ms(1);
+	if (machine == 0){		// CNC
+		PORTD |= dirY;
+		PORTB |= enY;
+		for(int i = 0; i < dist; i++){
+			PORTD |= clkY;
+			step_delay();
+			PORTD &= ~clkY;
+			step_delay();
 		}
-	} else if (machine == 1){		
-		PORTB =| dirY;
-		PORTB =| enY;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTC =| clkY;
-			_delay_ms(1);
-			PORTC =& ~clkY;
-			_delay_ms(1);
+	} else {		// Plotter branch (kept consistent)
+		PORTB |= dirY;
+		PORTB |= enY;
+		for(int i = 0; i < dist; i++){
+			PORTC |= clkY;
+			step_delay();
+			PORTC &= ~clkY;
+			step_delay();
 		}
 	}
-
 }
 
-
-
 void DOWN (int dist){
-	if (machine == 0){		// Chequea qué máquina es
-		PORTD =& ~dirY;
-		PORTB =| enY;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTD =| clkY;
-			_delay_ms(1);
-			PORTD =& ~clkY;
-			_delay_ms(1);
+	if (machine == 0){		// CNC
+		PORTD &= ~dirY;
+		PORTB |= enY;
+		for(int i = 0; i < dist; i++){
+			PORTD |= clkY;
+			step_delay();
+			PORTD &= ~clkY;
+			step_delay();
 		}
-	} else if (machine == 1){	
-		PORTD =& ~dirY;
-		PORTB =| enY;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTC =| clkY;
-			_delay_ms(1);
-			PORTC =& ~clkY;
-			_delay_ms(1);
+	} else {	// Plotter
+		PORTB &= ~dirY;
+		PORTB |= enY;
+		for(int i = 0; i < dist; i++){
+			PORTC |= clkY;
+			step_delay();
+			PORTC &= ~clkY;
+			step_delay();
 		}
 	}
-
 }
 
 //		Eje X
 /////////////////////
 
 void L (int dist){
-	if (machine == 0){		// Chequea qué máquina es
-		PORTD =& ~dirX;
-		PORTB =| enX;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTD =| clkX;
-			_delay_ms(1);
-			PORTD =& ~clkX;
-			_delay_ms(1);
+	if (machine == 0){		// CNC
+		PORTD &= ~dirX;      // direction = LEFT (0)
+		PORTB |= enX;
+		for(int i = 0; i < dist; i++){
+			PORTD |= clkX;
+			step_delay();
+			PORTD &= ~clkX;
+			step_delay();
 		}
-		
-	} else if (machine == 1){	
-		PORTB =& ~dirX;
-		PORTB =| enX;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTB =| clkX;
-			_delay_ms(1);
-			PORTB =& ~clkX;
-			_delay_ms(1);
+	} else {	// Plotter
+		PORTB &= ~dirX;
+		PORTB |= enX;
+		for(int i = 0; i < dist; i++){
+			PORTB |= clkX;
+			step_delay();
+			PORTB &= ~clkX;
+			step_delay();
 		}
 	}
 }
 
 void R (int dist){
-	if (machine == 0){		// Chequea qué máquina es
-		PORTD =& ~dirX;
-		PORTB =| enX;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTD =| clkX;
-			_delay_ms(1);
-			PORTD =& ~clkX;
-			_delay_ms(1);
+	if (machine == 0){		// CNC
+		PORTD |= dirX;       // direction = RIGHT (1)
+		PORTB |= enX;
+		for(int i = 0; i < dist; i++){
+			PORTD |= clkX;
+			step_delay();
+			PORTD &= ~clkX;
+			step_delay();
 		}
-		
-	} else if (machine == 1){	
-		PORTB =| dirX;
-		PORTB =| enX;
-		for(int i = 0 ; i <= dist ; i++){
-			PORTB =| clkX;
-			_delay_ms(1);
-			PORTB =& ~clkX;
-			_delay_ms(1);
+	} else {	// Plotter
+		PORTB |= dirX;
+		PORTB |= enX;
+		for(int i = 0; i < dist; i++){
+			PORTB |= clkX;
+			step_delay();
+			PORTB &= ~clkX;
+			step_delay();
 		}
 	}
 }
@@ -168,26 +147,20 @@ void R (int dist){
 /////////////////////
 
 void solD(void){
-	if (machine == 0){		// Chequea qué máquina es
+	if (machine == 0){		// CNC
 		PORTB |= sol;
 		_delay_ms(50);
-	}
-		
-	} else if (machine == 1){	
+	} else {	
 		PORTC |= sol;
 		_delay_ms(50);
 	}
 }
 
-
-
 void solU(void){
-	if (machine == 0){		// Chequea qué máquina es
+	if (machine == 0){		// CNC
 		PORTB &= ~sol;
 		_delay_ms(50);
-	}
-		
-	} else if (machine == 1){	
+	} else {	
 		PORTC &= ~sol;
 		_delay_ms(50);
 	}
@@ -195,204 +168,108 @@ void solU(void){
 
 void ledON(void){
 	if (machine == 0){		// CNC
-		PORTB |= led;
+		PORTB |= LED;
+	} else {	// Plotter lab
+		PORTD |= LED;
 	}
-		
-	} else if (machine == 1){	// Plotter lab
-		PORTD |= led;
-	}
-
 }
 
 void ledOFF(void){
 	if (machine == 0){		// CNC
-		PORTB &= ~led;
-	}
-		
-	} else if (machine == 1){	// Plotter lab
-		PORTD &= ~led;
+		PORTB &= ~LED;
+	} else {	// Plotter lab
+		PORTD &= ~LED;
 	}
 }
-
 
 //					Autohome
 //////////////////////////////////////////////////////
 void home(void){
-
 	if (machine == 0){		// CNC
 		uint16_t d = 0;
-	
-		
-		PORTB |= LED;			// Prende y apaga el LED
-		_delay_ms(300);
-		PORTB &= ~LED;
-		_delay_ms(150);
-		PORTB |= LED;
-		_delay_ms(300);
-		PORTB &= ~LED;
-		_delay_ms(200);
-	
+
+		// flash LED
+		PORTB |= LED; _delay_ms(300);
+		PORTB &= ~LED; _delay_ms(150);
+		PORTB |= LED; _delay_ms(300);
+		PORTB &= ~LED; _delay_ms(200);
+
 		solU();
-		
-		while(!(PINB & limYA || PINB & limYB)){		// Paso 1: Baja hasta encontrar el limite inferior (YA)
-				DOWN(1);
-		}
-	
-		_delay_ms(500);
-		
-		while(!(PINB & limYA || PINB & limYB)){		// Paso 1: Baja hasta encontrar el limite inferior (YA)
-				UP(1);
-				d++;
-		}
-		
-		_delay_ms(500);
-		
-		DOWN(d/2);	// Paso 3: Baja hasta la mitad del trayecto para ubicarse en el centro
-		
-	
-		PORTB |= LED;			// Prende y apaga el LED
-		_delay_ms(300);
-		PORTB &= ~LED;
-		_delay_ms(150);
-		PORTB |= LED;
-		_delay_ms(300);
-		PORTB &= ~LED;
-		_delay_ms(200);
 
-
-		
-	}
-
-
-
-
-
-
-
-
-	
-	} else if (machine == 1){	// Plotter lab
-		uint16_t d = 0;
-	
-		
-		PORTD |= LED;			// Prende y apaga el LED
-		_delay_ms(300);
-		PORTD &= ~LED;
-		_delay_ms(150);
-		PORTD |= LED;
-		_delay_ms(300);
-		PORTD &= ~LED;
-		_delay_ms(200);
-	
-		solU();
-		
-		while(!(PIND & limYA || PIND & limYB)){		// Paso 1: Baja hasta encontrar el limite inferior (YA)
-				DOWN(1);
-		}
-	
-		_delay_ms(500);
-		
-		while(!(PIND & limYA || PIND & limYB)){		// Paso 1: Baja hasta encontrar el limite inferior (YA)
-				UP(1);
-				d++;
-		}
-		
-		_delay_ms(500);
-		
-		DOWN(d/2);	// Paso 3: Baja hasta la mitad del trayecto para ubicarse en el centro
-		
-	
-		PORTD |= LED;			// Prende y apaga el LED
-		_delay_ms(300);
-		PORTD &= ~LED;
-		_delay_ms(150);
-		PORTD |= LED;
-		_delay_ms(300);
-		PORTD &= ~LED;
-		_delay_ms(200);
-
-	}
-
-
-
-	
-}
-
-
-
-
-
-//			Probador de frecuencias    (CÓDIGO VIEJO)
-//////////////////////////////////////////////////////
-
-void pruebaFrecuencias(void){
-	for (uint16_t time = 2 ; time <= 10 ; time++){		// De 500 Hz a 100 Hz
-		for (uint8_t rep = 0; rep < 20 ; rep++){			// Da 20 pasos con la frecuencia estimada
-			PORTC |= PASO1UP;			// Pone en 1 todos los bits que mueven el motor
-			for (uint8_t n = 0 ; n < time ; n++){
-				_delay_ms(1);
-			}
-			PORTC &= 0xF7;				// ~0b0000 1000 (CLK Y) Apaga la señal de reloj
-			for (uint8_t n = 0 ; n < time ; n++){
-				_delay_ms(1);
-			}
-		}
-	}
-
-}
-
-
-
-//					Figuras
-//////////////////////////////////////////////////////
-//		Circulo
-/////////////////////
-void circle (uint8_t radius){
-	solU();
-	L(radius);
-	solD();
-
-	for (uint8_t x = 0 ; x < radius ; x++){			// Cuadrante 1
-		R(1);
-		for (uint8_t j = 0; j <= sqrt((pow(radius,2))-(pow((x-radius),2))) ; j++){
+		// Step down until lower-limit triggered (assumes active-high)
+		while(!(PINB & limYA)){
 			UP(1);
 		}
-	}
 
-	for (uint8_t x = 0 ; x < radius ; x++){			// Cuadrante 2
-		R(1);
-		DOWN(sqrt((pow(radius,2))-(pow((x-radius),2))));
-	}
+		_delay_ms(500);
 
-	for (uint8_t x = 0 ; x < radius ; x++){			// Cuadrante 3
-		L(1);
-		DOWN(sqrt((pow(radius,2))-(pow((x-radius),2))));
-	}
+		// Step up and count until limit again (measure travel)
+		while(!(PINB & limYD)){
+			DOWN(1);
+			d++;
+		}
 
-	for (uint8_t x = 0 ; x < radius ; x++){			// Cuadrante 4
-		L(1);
-		UP(sqrt((pow(radius,2))-(pow((x-radius),2))));
-	}
+		_delay_ms(500);
 
-	
+		if (d > 0) UP(d/2);	// go to middle
+
+		// flash LED end
+		PORTB |= LED; _delay_ms(300);
+		PORTB &= ~LED; _delay_ms(150);
+		PORTB |= LED; _delay_ms(300);
+		PORTB &= ~LED; _delay_ms(200);
+	} else {	// Plotter lab
+		uint16_t d = 0;
+
+		PORTD |= LED; _delay_ms(300);
+		PORTD &= ~LED; _delay_ms(150);
+		PORTD |= LED; _delay_ms(300);
+		PORTD &= ~LED; _delay_ms(200);
+
+		solU();
+
+		while(!(PIND & limYA) && !(PIND & limYD)){
+			DOWN(1);
+		}
+
+		_delay_ms(500);
+
+		while(!(PIND & limYA) && !(PIND & limYD)){
+			UP(1);
+			d++;
+		}
+
+		_delay_ms(500);
+
+		if (d > 0) DOWN(d/2);
+
+		PORTD |= LED; _delay_ms(300);
+		PORTD &= ~LED; _delay_ms(150);
+		PORTD |= LED; _delay_ms(300);
+		PORTD &= ~LED; _delay_ms(200);
+	}
 }
 
+/*
+void circle(uint8_t radius){
+
+}
+*/
 
 void cuadrado(uint16_t tam){
-	solD;
+	solD();
 	L(tam/2);
 	UP(tam/2);
 	R(tam);
 	DOWN(tam);
 	L(tam);
 	UP(tam/2);
-	solUP;
-	R(tam/2)
+	solU();
+	R(tam/2);
 }
 
-
 void cruz(uint16_t tam){
-	solD;
+	solD();
 	UP(tam/3);
 	DOWN(tam/3);
 	R(tam/3);
@@ -401,58 +278,61 @@ void cruz(uint16_t tam){
 	R(tam/3);
 	DOWN(tam*2/3);
 	UP(tam*2/3);
-	solU;
+	solU();
 }
-
-
-
-
-
-
-
-
-
 
 //										MAIN
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 int main (void){
-	
-	ledON();
-	_delay_ms(50);
-	ledOFF();
-	_delay_ms(50);
-	ledON();
-	_delay_ms(50);
-	ledOFF();
-	_delay_ms(50);
-	ledON();
-	_delay_ms(50);
-	ledOFF();
-	_delay_ms(50);
-	ledON();
-	_delay_ms(50);
-	ledOFF();
-	_delay_ms(50);
+	if (machine == 0){				// CNC
+	    // Configure outputs: PD2 clkX, PD3 clkY, PD5 dirX, PD6 dirY
+	    DDRD |= clkX | clkY | dirX | dirY;
+	    // PortB: PB0 en, PB3 solenoid, PB5 LED output; PB1/PB2 inputs for limits
+	    DDRB |= enX | sol | LED;
+	    DDRB &= ~(limYA | limYD); 	// limit switches as INPUT
+	    PORTB |= limYA | limYD; 	// enable pull-ups on switches
+	} else {	// Plotter lab
+	    DDRD |= clkX | clkY | dirX | dirY;
+	    DDRB |= enX | sol | LED;
+	    DDRB &= ~(limYA | limYD);
+	    PORTB |= limYA | limYD;
+	}
 
+	// initial states
+	PORTD &= ~(clkX | clkY | dirX | dirY);
+	PORTB &= ~sol;
+	PORTB &= ~enX;
+	PORTB &= ~LED;
 
+	// blink startup
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+  	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+  	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+  	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+  	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
+	ledON(); _delay_ms(50); ledOFF(); _delay_ms(50);
 
+	// quick motion test (safe)
+	//UP(2000); _delay_ms(2000); 	UP(200); _delay_ms(200); 
+	//DOWN(200); _delay_ms(200); 	DOWN(200); _delay_ms(200);
+	//L(200);
+	//R(200);
 
-
-	
-	UP(200);
-	_delay_ms(200);
-	DOWN(200);
-	L(200);
-	R(200);
-
-
-
-	
-	
 	home();
 
-	
+	while (1){
+		_delay_ms(1000);
+	}
 
-	
+	return 0;
 }
